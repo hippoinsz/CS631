6bbe792db2bf6c1f7d91a36f30a6e6108a4f9ba8
Fubin Mo <mofubin2@outlook.com>
2020-10-19 16:10:12 -0400

    Create print.h

---
+++ midterm/print.h
@@ -0,0 +1,20 @@
+//
+//  print.h
+//  test
+//
+//  Created by FuBin Mo on 10/18/20.
+//
+
+#ifndef print_h
+#define print_h
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <grp.h>
+#include <pwd.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include<string.h>
+
+#endif /* print_h */
